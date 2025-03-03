ca0f4ce8c9215576bc4daf9825cb078b2aff55c6
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-03-03 20:53:17 +0600

    added thief catch project

---
+++ thiefDetector.ino
@@ -0,0 +1,21 @@
+// purpose: to detect a thief using a PIR sensor and sound an alarm using a buzzer
+const int buzzerInputPin = 13;
+const int PIROutputPin = 12;
+
+
+void setup(){
+   
+
+    pinMode(buzzerInputPin, OUTPUT);
+    pinMode(PIROutputPin, INPUT);
+    
+}
+
+void loop(){
+    if (digitalRead(PIROutputPin)){
+        digitalWrite(buzzerInputPin, HIGH);
+        delay(5000);
+        digitalWrite(buzzerInputPin, LOW);
+    }
+    else digitalWrite(buzzerInputPin, LOW);
+}   
